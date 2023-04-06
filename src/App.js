fe2ced03dea0a5fb16be0b46271fab211c7cae2d
Romain86 <rmartinet77@gmail.com>
2023-04-06 17:29:14 +0200

    Création de formulaire en React

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ function App() {
 		{ id: 3, nom: "Cerise" }
 	]);
 
+	const [nouveauFruit, setNouveauFruit] = useState("");
+
+	// const inputRef = useRef();
+
 
 	// comportements
 	const HandleDelete = (id) => {
@@ -18,10 +22,35 @@ function App() {
 		// const fruitsCopy = fruits.slice();
 
 		// manipuler la copie du state
-		const fruitsCopyUpdated =  fruitsCopy.filter(fruit => fruit.id !== id)
+		const fruitsCopyUpdated = fruitsCopy.filter(fruit => fruit.id !== id)
 
 		// modifier le state avec setter
 		setFruits(fruitsCopyUpdated);
+	}
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		// alert("handleSubmit");
+		// console.log(inputRef.current.value);
+		
+		// copie du state
+		const fruitsCopy = [...fruits];
+		
+		// manipulation sur la copie du state
+		const id = new Date().getTime();
+		const nom = nouveauFruit;
+		fruitsCopy.push({ id, nom});
+		// console.log(fruitsCopy);
+
+		// modifier le state avec setter
+		setFruits(fruitsCopy);
+		setNouveauFruit("");
+	}
+
+	const heandleChange = (event) => {
+		// console.log(valueAfterChange);
+		setNouveauFruit(event.target.value);
+
 	}
 
 	// affichage (render)
@@ -32,8 +61,25 @@ function App() {
 				<li key={fruit.id}>
 					{fruit.nom} <button onClick={() => HandleDelete(fruit.id)}>X</button>
 				</li>
-		))}</ul>
+			))}</ul>
+		<form action="submit" onSubmit={handleSubmit}>
+			<input 
+				value={nouveauFruit} 
+				// ref={inputRef}
+				type="text" 
+				placeholder="Ajouter un fruit..." 
+				onChange={heandleChange}
+				/>
+			<button>Ajouter +</button>
+		</form>
 	</div>
 }
 
 export default App;
+
+// Gestion du formulaire
+// 1. Création du formulaire
+// 2. Soumission du formulaire (onSubmit / handleSubmit)
+// 3. Collecte des données du formulaire
+// 3a. méthode 1 : documentGetElementById "React" (useRef)
+// 3b. méthode 2 : sync descente / acendante (onChange / handleChange)
